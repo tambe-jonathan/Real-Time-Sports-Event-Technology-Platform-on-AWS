950695451f07c6c9274ad63154f702ed18da5f7d
agbor jonathan tambe <155083485+tambe-jonathan@users.noreply.github.com>
2026-05-20 00:20:09 +0300

    Update backend.tf

--- infrastructure/environments/dev/backend.tf
+++ infrastructure/environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-#  backend "s3" {
-#    bucket         = "eventpulse-terraform-state-2026" 
-#    key            = "dev/eventpulse-infra.tfstate"
-#    region         = "us-east-1"
-#    dynamodb_table = "eventpulse-terraform-locks"       
-#    encrypt        = true
-#  }
+  backend "s3" {
+    bucket         = "eventpulse-terraform-state-2026"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "eventpulse-terraform-locks"
+    encrypt        = true
+  }
 }
